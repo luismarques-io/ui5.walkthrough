1b7855c635027df6599b7138d89dfde99cdf8aad
Luís Marques <l.marques@sap.com>
2024-10-20 19:13:23 +0100

    Refactor Component initialization and update manifest for i18n support; remove obsolete index files

--- webapp/index.ts
+++
@@ -1,9 +0,0 @@
-import ComponentContainer from "sap/ui/core/ComponentContainer";
-
-new ComponentContainer({
-  name: "ui5.walkthrough",
-  settings: {
-    id: "walkthroughts"
-  },
-  async: true
-}).placeAt("content");
