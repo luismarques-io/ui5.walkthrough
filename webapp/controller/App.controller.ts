a89c35ee2c320ed0735842f466968e2d48e37e94
Luís Marques <l.marques@sap.com>
2024-10-20 18:38:15 +0100

    Add i18n support to App controller and create i18n properties file

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -1,6 +1,8 @@
+import ResourceBundle from "sap/base/i18n/ResourceBundle";
 import MessageToast from "sap/m/MessageToast";
 import Controller from "sap/ui/core/mvc/Controller";
 import JSONModel from "sap/ui/model/json/JSONModel";
+import ResourceModel from "sap/ui/model/resource/ResourceModel";
 
 /**
  * @name ui5.walkthrough.controller.App
@@ -14,10 +16,22 @@ export default class AppController extends Controller {
       }
     };
     const dataModel = new JSONModel(data);
+    // because of "strict" mode in tsconfig.json a null check is required for this.getView()
     this.getView()?.setModel(dataModel);
+
+    // set i18n model on view
+    const i18nModel = new ResourceModel({
+      bundleName: "ui5.walkthrough.i18n.i18n"
+    });
+    this.getView()?.setModel(i18nModel, "i18n");
   }
 
   onShowHello(): void {
-    MessageToast.show("Hello World");
+    // read msg from i18n model
+    const recipient = (<JSONModel>this.getView()?.getModel())?.getProperty("/recipient/name");
+    const resourceBundle = <ResourceBundle>(<ResourceModel>this.getView()?.getModel("i18n"))?.getResourceBundle();
+    const msg = resourceBundle.getText("helloMsg", [recipient]) || "no text defined";
+    // show message
+    MessageToast.show(msg);
   }
 }
