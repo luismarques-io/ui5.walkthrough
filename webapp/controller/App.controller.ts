a7e784e6d0b13f52004788e21bf5b47ae6a60268
Luís Marques <l.marques@sap.com>
2024-10-20 18:46:52 +0100

    Refactor application structure to use UIComponent and ComponentContainer; move data and i18n model initialization to Component

--- webapp/controller/App.controller.ts
+++ webapp/controller/App.controller.ts
@@ -8,24 +8,6 @@ import ResourceModel from "sap/ui/model/resource/ResourceModel";
  * @name ui5.walkthrough.controller.App
  */
 export default class AppController extends Controller {
-  onInit(): void {
-    // set data model on view
-    const data = {
-      recipient: {
-        name: "World"
-      }
-    };
-    const dataModel = new JSONModel(data);
-    // because of "strict" mode in tsconfig.json a null check is required for this.getView()
-    this.getView()?.setModel(dataModel);
-
-    // set i18n model on view
-    const i18nModel = new ResourceModel({
-      bundleName: "ui5.walkthrough.i18n.i18n"
-    });
-    this.getView()?.setModel(i18nModel, "i18n");
-  }
-
   onShowHello(): void {
     // read msg from i18n model
     const recipient = (<JSONModel>this.getView()?.getModel())?.getProperty("/recipient/name");
