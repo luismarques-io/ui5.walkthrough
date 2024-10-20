8630f8066da14e1fa368bcadd962753dc1975c4e
Luís Marques <l.marques@sap.com>
2024-10-20 18:21:56 +0100

    Add OpenUI5 controller and view, update index.html and TypeScript setup

--- webapp/index.ts
+++ webapp/index.ts
@@ -1,1 +1,7 @@
-alert("UI5 is ready");
+import XMLView from "sap/ui/core/mvc/XMLView";
+
+XMLView.create({
+  viewName: "ui5.walkthrough.view.App"
+}).then(function (view) {
+  view.placeAt("content");
+});
