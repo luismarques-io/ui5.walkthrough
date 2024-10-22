5defb8c378da297b30b790a77c18a09c8bcf6484
Luís Marques <l.marques@sap.com>
2024-10-22 15:16:32 +0100

    Add navigation back functionality to Detail view; update controller and XML view

--- webapp/controller/Detail.controller.ts
+++ webapp/controller/Detail.controller.ts
@@ -1,4 +1,5 @@
 import Controller from "sap/ui/core/mvc/Controller";
+import History from "sap/ui/core/routing/History";
 import { Route$PatternMatchedEvent } from "sap/ui/core/routing/Route";
 import Component from "../Component";
 
@@ -16,5 +17,17 @@ export default class Detail extends Controller {
             path: "/" + window.decodeURIComponent((<any>event.getParameter("arguments")).invoicePath),
             model: "invoice",
         });
+    }
+
+    onNavBack(): void {
+        const history = History.getInstance();
+        const previousHash = history.getPreviousHash();
+
+        if (previousHash !== undefined) {
+            window.history.go(-1);
+        } else {
+            const router = (<Component>this.getOwnerComponent()).getRouter();
+            router.navTo("overview", {}, true);
+        }
     }
 }
